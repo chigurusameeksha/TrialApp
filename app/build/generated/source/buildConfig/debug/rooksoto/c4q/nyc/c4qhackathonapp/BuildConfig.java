ad7f5f574233d63b28f92b53e1507be0979544cf
chigurusameeksha <sameekshachiguru@gmail.com>
2020-10-05 09:03:16 +0530

    initial

---
+++ app/build/generated/source/buildConfig/debug/rooksoto/c4q/nyc/c4qhackathonapp/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package rooksoto.c4q.nyc.c4qhackathonapp;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "rooksoto.c4q.nyc.c4qhackathonapp";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
